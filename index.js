0cf9d86d46133fd4c5d2d87172987cf2a51adc57
Dineshkumar510 <dk6089567@gmail.com>
2021-05-20 21:52:33 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+const path = require('path');
+const port = process.env.PORT || 8060;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+
+app.post('/',(req, res)=> {
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.USERNAME,
+            pass: process.env.PASSWORD
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: process.env.USERNAME,
+        subject: `Message from ${req.body.email}: from phone "${req.body.phone}"`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info)=> {
+        if(error){
+            console.log(error);
+            res.send("Something went Wrong, Check in Backend");
+        }
+        else{
+            console.log('Email sent' + info); 
+            res.send("success");
+        }
+    })
+
+})
+
+
+app.listen(port, ()=> {
+    console.log(`App is running on port: ${port}`);
+});
---
+++ public/js/main.js
@@ -0,0 +1,102 @@
+
+
+//Email  form
+
+
+const ContactForm = document.querySelector('.contact-form');
+
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let message = document.getElementById('message');
+
+
+ContactForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    let FormData = {
+      name:name.value, 
+      email:email.value, 
+      phone:phone.value,
+      message:message.value
+    }
+  
+    let xhr = new XMLHttpRequest();
+  xhr.open('POST','/');
+  xhr.setRequestHeader('content-type','application/json');
+  xhr.onload = function(){
+      console.log(xhr.responseText);
+      if(xhr.responseText == 'success'){
+        alert("Email Send Successfully!");
+          name.value = '';
+          email.value = '';
+          phone.value = '';
+          message.value = '';
+      }
+      else {
+          alert("something went wrong!");
+      }
+  }
+
+  xhr.send(JSON.stringify(FormData));
+
+})
+
+
+
+// slider 
+
+ // Initialize Swiper 
+ 
+ var swiper = new Swiper(".mySwiper", {
+   effect: "coverflow",
+   grabCursor: true,
+   centeredSlides: true,
+   slidesPerView: "auto",
+   coverflowEffect: {
+     rotate: 50,
+     stretch: 0,
+     depth: 100,
+     modifier: 1,
+     slideShadows: true,
+   },
+   loop: true,
+   autoplay : {
+     delay: 2500,
+     disableOnInteraction: false,
+   },
+   pagination: {
+     el: ".swiper-pagination",
+   },
+ });
+
+
+
+//bottom up 
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 200) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '200');
+});
+
+
+// age 
+
+var birth = new Date('1997-07-24');
+var now = new Date();
+var difference = now - birth;
+// Dates are set from 1970, so remove that to get the year 
+var age = new Date(difference).getFullYear() - 1970
+
+var span = document.getElementById('age');
+span.innerHTML = age;
