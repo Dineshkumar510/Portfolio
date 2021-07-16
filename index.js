90f50ad6ba1f21c74c9730cf1f4f5dea7235f16f
Dineshkumar510 <dk6089567@gmail.com>
2021-07-17 00:00:48 +0530

    Making better

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.post('/',(req, res)=> {
     console.log(req.body);
 
     const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         service: 'gmail',
         auth: {
             user: process.env.USERNAME || "DineshMails510@gmail.com",
